bcfaf22db517e5661e1a5b0144550c365455216c
siyana-beta <siyana.ecosystem@gmail.com>
2026-05-27 04:35:41 +0300

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,3 +0,0 @@
-import './globals.css'
-export const metadata = { title: 'SIYANA Beta', description: 'Understand your body' }
-export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="ru"><body>{children}</body></html>}
